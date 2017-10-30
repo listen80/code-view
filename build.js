23c035429ee1c10d6416f2eeffdc3d20156018f6
jiabinli <jiabinli@tencent.com>
2017-10-30 19:52:02 +0800

    一次大升级

---
+++ build.js
@@ -0,0 +1,27 @@
+const uglify = require('uglify-js');
+const clean = require('clean-css');
+const fs = require('fs');
+const pack = require('./package.json');
+
+
+// js
+let js = fs.readFileSync('./src/code.js').toString();
+const result = uglify.minify(js);
+if(result.error) {
+    console.log(result.error);
+} else {
+    fs.writeFileSync('./dist/code.js', result.code);
+}
+
+
+// css
+let css = fs.readFileSync('./code.css').toString();
+const min = new clean().minify(css);
+if(min.errors.length) {
+    console.log(min.errors);
+} else {
+    fs.writeFileSync('./dist/code.css', min.styles.toString());
+}
+
+let msg = `${pack.name} ${pack.version} is built @ ${new Date().toLocaleString()}`
+console.log(msg);
