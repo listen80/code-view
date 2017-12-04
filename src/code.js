fc754a971651e2361d9437d439f769dea6620cf4
jiabinli <jiabinli@tencent.com>
2017-12-04 21:43:33 +0800

    fixed ie8 array.indexOf bug

--- src/code.js
+++ src/code.js
@@ -11,6 +11,8 @@
         yellow = 'yellow',
         ryan = 'ryan';
 
+
+
     var token, style, cache, match, i, analysis;
 
     var keywords = 'break,case,catch,const,continue,debugger,default,delete,do,else,extends,finally,for,if,import,in,instanceof,let,new,return,super,switch,throw,try,typeof,var,void,while,with'.split(',');
@@ -20,6 +22,14 @@
     var cssKeys = 'align-content,align-items,align-self,alignment-baseline,all,animation,animation-delay,animation-direction,animation-duration,animation-fill-mode,animation-iteration-count,animation-name,animation-play-state,animation-timing-function,backface-visibility,background,background-attachment,background-blend-mode,background-clip,background-color,background-image,background-origin,background-position,background-position-x,background-position-y,background-repeat,background-repeat-x,background-repeat-y,background-size,baseline-shift,border,border-bottom,border-bottom-color,border-bottom-left-radius,border-bottom-right-radius,border-bottom-style,border-bottom-width,border-collapse,border-color,border-image,border-image-outset,border-image-repeat,border-image-slice,border-image-source,border-image-width,border-left,border-left-color,border-left-style,border-left-width,border-radius,border-right,border-right-color,border-right-style,border-right-width,border-spacing,border-style,border-top,border-top-color,border-top-left-radius,border-top-right-radius,border-top-style,border-top-width,border-width,bottom,box-shadow,box-sizing,break-after,break-before,break-inside,buffered-rendering,caption-side,clear,clip,clip-path,clip-rule,color,color-interpolation,color-interpolation-filters,color-rendering,column-count,column-fill,column-gap,column-rule,column-rule-color,column-rule-style,column-rule-width,column-span,column-width,columns,contain,content,counter-increment,counter-reset,cursor,cx,cy,d,direction,display,dominant-baseline,empty-cells,fill,fill-opacity,fill-rule,filter,flex,flex-basis,flex-direction,flex-flow,flex-grow,flex-shrink,flex-wrap,float,flood-color,flood-opacity,font,font-family,font-feature-settings,font-kerning,font-size,font-stretch,font-style,font-variant,font-variant-caps,font-variant-ligatures,font-variant-numeric,font-weight,height,hyphens,image-rendering,isolation,justify-content,left,letter-spacing,lighting-color,line-height,list-style,list-style-image,list-style-position,list-style-type,margin,margin-bottom,margin-left,margin-right,margin-top,marker,marker-end,marker-mid,marker-start,mask,mask-type,max-height,max-width,max-zoom,min-height,min-width,min-zoom,mix-blend-mode,motion,object-fit,object-position,offset,offset-distance,offset-path,offset-rotation,opacity,order,orientation,orphans,outline,outline-color,outline-offset,outline-style,outline-width,overflow,overflow-wrap,overflow-x,overflow-y,padding,padding-bottom,padding-left,padding-right,padding-top,page,page-break-after,page-break-before,page-break-inside,paint-order,perspective,perspective-origin,pointer-events,position,quotes,r,resize,right,rx,ry,shape-image-threshold,shape-margin,shape-outside,shape-rendering,size,speak,src,stop-color,stop-opacity,stroke,stroke-dasharray,stroke-dashoffset,stroke-linecap,stroke-linejoin,stroke-miterlimit,stroke-opacity,stroke-width,tab-size,table-layout,text-align,text-align-last,text-anchor,text-combine-upright,text-decoration,text-indent,text-orientation,text-overflow,text-rendering,text-shadow,text-size-adjust,text-transform,top,touch-action,transform,transform-origin,transform-style,transition,transition-delay,transition-duration,transition-property,transition-timing-function,unicode-bidi,unicode-range,user-select,user-zoom,vector-effect,vertical-align,visibility,webkit-app-region,webkit-appearance,webkit-background-clip,webkit-background-origin,webkit-border-after,webkit-border-after-color,webkit-border-after-style,webkit-border-after-width,webkit-border-before,webkit-border-before-color,webkit-border-before-style,webkit-border-before-width,webkit-border-end,webkit-border-end-color,webkit-border-end-style,webkit-border-end-width,webkit-border-horizontal-spacing,webkit-border-image,webkit-border-start,webkit-border-start-color,webkit-border-start-style,webkit-border-start-width,webkit-border-vertical-spacing,webkit-box-align,webkit-box-decoration-break,webkit-box-direction,webkit-box-flex,webkit-box-flex-group,webkit-box-lines,webkit-box-ordinal-group,webkit-box-orient,webkit-box-pack,webkit-box-reflect,webkit-column-break-after,webkit-column-break-before,webkit-column-break-inside,webkit-font-size-delta,webkit-font-smoothing,webkit-highlight,webkit-hyphenate-character,webkit-line-break,webkit-line-clamp,webkit-locale,webkit-logical-height,webkit-logical-width,webkit-margin-after,webkit-margin-after-collapse,webkit-margin-before,webkit-margin-before-collapse,webkit-margin-bottom-collapse,webkit-margin-collapse,webkit-margin-end,webkit-margin-start,webkit-margin-top-collapse,webkit-mask,webkit-mask-box-image,webkit-mask-box-image-outset,webkit-mask-box-image-repeat,webkit-mask-box-image-slice,webkit-mask-box-image-source,webkit-mask-box-image-width,webkit-mask-clip,webkit-mask-composite,webkit-mask-image,webkit-mask-origin,webkit-mask-position,webkit-mask-position-x,webkit-mask-position-y,webkit-mask-repeat,webkit-mask-repeat-x,webkit-mask-repeat-y,webkit-mask-size,webkit-max-logical-height,webkit-max-logical-width,webkit-min-logical-height,webkit-min-logical-width,webkit-padding-after,webkit-padding-before,webkit-padding-end,webkit-padding-start,webkit-perspective-origin-x,webkit-perspective-origin-y,webkit-print-color-adjust,webkit-rtl-ordering,webkit-ruby-position,webkit-tap-highlight-color,webkit-text-combine,webkit-text-decorations-in-effect,webkit-text-emphasis,webkit-text-emphasis-color,webkit-text-emphasis-position,webkit-text-emphasis-style,webkit-text-fill-color,webkit-text-orientation,webkit-text-security,webkit-text-stroke,webkit-text-stroke-color,webkit-text-stroke-width,webkit-transform-origin-x,webkit-transform-origin-y,webkit-transform-origin-z,webkit-user-drag,webkit-user-modify,webkit-writing-mode,white-space,widows,width,will-change,word-break,word-spacing,word-wrap,writing-mode,x,y,z-index,zoom'.split(',');
 
     var cssValues = 'red,blue,yellow,green,lime,' + 'content-box,border-box,' + 'translateX,translateY,translateZ,' + 'url,rgba,rgb' + 'whitespace,wait,w-resize,visible,vertical-text,vertical-ideographic,uppercase,upper-roman,upper-alpha,underline,transparent,top,thin,thick,text,text-top,text-bottom,tb-rl,table-header-group,table-footer-group,sw-resize,super,strict,static,square,solid,small-caps,separate,se-resize,scroll,s-resize,rtl,row-resize,ridge,right,repeat,repeat-y,repeat-x,relative,progress,pointer,overline,outside,outset,oblique,nowrap,not-allowed,normal,none,nw-resize,no-repeat,no-drop,newspaper,ne-resize,n-resize,move,middle,medium,ltr,lr-tb,lowercase,lower-roman,lower-alpha,loose,list-item,line,line-through,line-edge,lighter,left,keep-all,justify,italic,inter-word,inter-ideograph,inside,inset,inline,inline-block,inherit,inactive,ideograph-space,ideograph-parenthesis,ideogwhitespace,wait,w-resize,visible,vertical-text,vertical-ideographic,uppercase,upper-roman,upper-alpha,underline,transparent,top,thin,thick,text,text-top,text-bottom,tb-rl,table-header-group,table-footer-group,sw-resize,super,strict,static,square,solid,small-caps,separate,se-resize,scroll,s-resize,rtl,row-resize,ridge,right,repeat,repeat-y,repeat-x,relative,progress,pointer,overline,outside,outset,oblique,nowrap,not-allowed,normal,none,nw-resize,no-repeat,no-drop,newspaper,ne-resize,n-resize,move,middle,medium,ltr,lr-tb,lowercase,lower-roman,lower-alpha,loose,list-item,line,line-through,line-edge,lighter,left,keep-all,justify,italic,inter-word,inter-ideograph,inside,inset,inline,inline-block,inherit,inactive,ideograph-space,ideograph-parenthesis,ideograph-numeric,ideograph-alpha,horizontal,hidden,help,hand,groove,fixed,ellipsis,e-resize,double,dotted,distribute,distribute-space,distribute-letter,distribute-all-lines,disc,disabled,default,decimal,dashed,crosshair,collapse,col-resize,circle,char,center,capitalize,break-word,break-all,bottom,both,bolder,bold,block,bidi-override,below,baseline,auto,always,all-scroll,absolute,table,table-cell-all,bottom,both,bolder,bold,block,bidi-override,below,baseline,auto,always,all-scroll,absolute,table,table-cell,table-row'.split(',');
+
+    function in_array(value, arr) {
+        for (var x = 0, len = arr.length; x < len; x++) {
+            if (arr[x] === value) {
+                return 1;
+            }
+        }
+    }
 
     function push() {
         analysis.push([cache, style])
@@ -58,23 +68,27 @@
     }
 
     function isKeyword() {
-        return keywords.indexOf(cache) !== -1;
+        return in_array(cache, keywords);
     }
 
     function isConstant() {
-        return ['null', 'true', 'false', 'undefined'].indexOf(cache) !== -1;
+        return in_array(cache, ['null', 'true', 'false', 'undefined']);
     }
 
     function isVariable() {
         return cache === 'var' || cache === 'let' || cache === 'const';
     }
 
+    var parameter = ['self', 'this', 'argument']
+
     function isParameter() {
-        return cache === 'self' || cache === 'this' || cache === 'argument';
+        return in_array(cache, parameter);
     }
 
+    var normal = ['prototype', 'module', 'window', 'document', 'history', 'location', 'screen', 'console', 'Object', 'Array', 'Number', 'Boolean', 'String', 'RegExp', 'Math'];
+
     function isNormal() {
-        return ['prototype', 'module', 'window', 'document', 'history', 'location', 'screen', 'console', 'Object', 'Array', 'Number', 'Boolean', 'String', 'RegExp', 'Math'].indexOf(cache) !== -1;
+        return in_array(cache, normal);
     }
 
     function getHex() {
@@ -138,15 +152,15 @@
     }
 
     function isTargetName() {
-        return ~tagnames.indexOf(cache);
+        return in_array(cache, tagnames);
     }
 
     function isStyleKey() {
-        return ~cssKeys.indexOf(cache.replace(/^-(webkit|moz|ms|o)-/, ''));
+        return in_array(cache.replace(/^-(webkit|moz|ms|o)-/, ''), cssKeys);
     }
 
     function isCssValue() {
-        return ~cssValues.indexOf(cache.replace(/^-(webkit|moz|ms|o)-/, ''));
+        return in_array(cache.replace(/^-(webkit|moz|ms|o)-/, ''), cssValues);
     }
 
     function handle(style) {
@@ -323,9 +337,9 @@
                             i += 2;
                         }
                     } else if (isHtmlLetter()) {
-                        if(token === '-') {
+                        if (token === '-') {
                             token = source[i + 1];
-                            if(isNumber() || token === '.') {
+                            if (isNumber() || token === '.') {
                                 token = '-';
                                 handle(purple);
                                 continue;
@@ -817,23 +831,24 @@
 
         function handleFunction() {
             var len = analysis.length - 1;
-            token = analysis[len][0][0];
-            if (isSpace()) {
-                len--;
-                token = analysis[len][0][0];
-            }
-            if (token === ':' || token === '=') {
-                len--;
+            if(len > -1) {
                 token = analysis[len][0][0];
                 if (isSpace()) {
                     len--;
                     token = analysis[len][0][0];
                 }
-                if (isLetter()) {
-                    analysis[len][1] = 'green';
+                if (token === ':' || token === '=') {
+                    len--;
+                    token = analysis[len][0][0];
+                    if (isSpace()) {
+                        len--;
+                        token = analysis[len][0][0];
+                    }
+                    if (isLetter()) {
+                        analysis[len][1] = 'green';
+                    }
                 }
             }
-
             token = source[i];
             cache = 'function';
             style = ryan + italic;
@@ -931,8 +946,8 @@
     }
 
     function AnalysisForElement(script) {
-        if (!script.code) {
-            script.code = true;
+        if (!script.coded) {
+            script.coded = true;
             var firstChild = script.firstChild;
             if (firstChild) {
                 var codes = AnalysisFor(firstChild.nodeValue.replace(/^\s+|\s+$/g, ""));
@@ -987,6 +1002,6 @@
         this.code = code;
     }
 
-    document && setTimeout(code);
+    document && setTimeout(code, 222);
 
 }(this.document)
