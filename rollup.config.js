df3059a3fa4531075a7c80309653ee5569370be8
jiabin.li <jiabin.li@shopee.com>
2022-09-28 18:18:37 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from "rollup-plugin-terser"; // 压缩代码
 import serve from "rollup-plugin-serve"; // 启动服务
 
 import replace from "rollup-plugin-replace"; // 注入环境变量
+
 const plugins = [
   commonjs(),
   resolve({
@@ -21,7 +22,7 @@ if (process.env.NODE_ENV === "development") {
     serve({
       open: true,
       // Remember to start with a slash.
-      openPage: '/examples/',
+      // openPage: '/examples/',
       port: 9000,
       contentBase: "",
     })
